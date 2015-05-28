ec66ec22fac1649cbd9fd7e1b6bcf438466e6eee
dhruvarora <dhruvarora@berkeley.edu>
2015-05-28 15:30:29 -0700

    Add background, quit event, better func flow

--- battleship.py
+++ battleship.py
@@ -3,17 +3,47 @@
 #Import Libraries
 import pygame, math, sys
 from pygame.locals import *
-screen = pygame.display.set_mode((1024, 768))
-clock = pygame.time.Clock()
 
-BOARDHEIGHT = 10
+# Globals
 BOARDWIDTH = 10
+BOARDHEIGHT = 10
+WINDOWWIDTH = 1024
+WINDOWHEIGHT = 768
+FPS = 30
+BLUE = (41, 128, 185)
+SHIPS = ['battleship', 'cruiser', 'destroyer', 'submarine', 'dinghy']
 
 def main():
+    global FPSCLOCK, SCREEN, BOARDWIDTH, BOARDHEIGHT, WINDOWWIDTH, WINDOWHEIGHT, FPS, BLUE, \
+            SHIPS
     pygame.init()
+    FPSCLOCK = pygame.time.Clock()
+    SCREEN = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
     pygame.display.set_caption('Battleship')
-    print(generate_board(False))
+    while True:
+        run_game()
 
+def run_game():
+    """
+    This function runs the main event processing loop for the game
+    """
+
+    # ----- MAIN PROGRAM LOOP ------ #
+    while True:
+        SCREEN.fill(BLUE)
+        check_for_quit()
+        mouse_clicked_flag = False
+
+        # --- MAIN EVENT LOOP ----- #
+        for event in pygame.event.get():
+            if event.type == MOUSEBUTTONUP:
+                print 'MOUSE BUTTON UP'
+
+        # --- UPDATE SCREEN ----
+        pygame.display.update()
+
+        # --- LIMIT TO FPS ----
+        FPSCLOCK.tick(FPS)
 
 
 def generate_board(value):
@@ -22,6 +52,14 @@ def generate_board(value):
     """
     board = [[value] * BOARDWIDTH for i in range(BOARDHEIGHT)]
     return board
+
+
+
+def check_for_quit():
+    for event in pygame.event.get(QUIT):
+        pygame.quit()
+        sys.exit()
+
 
 #Start everything off
 if __name__ == '__main__':
